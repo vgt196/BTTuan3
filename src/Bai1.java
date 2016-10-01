271f2040c6330b6a54d909c0d8163e39378174da
Trương Phùng Khánh Duy <khduy96@gmail.com>
2016-10-01 15:26:47 +0700

    Update Bai1.java

--- src/Bai1.java
+++ src/Bai1.java
@@ -8,3 +8,98 @@ public class Bai2 {
 	public static void main(String[] args) {
         System.out.println("Hello World!")
 }
+
+<html>
+  <head>
+<title>Tạp Chí KHSPKT</title></head>
+
+<link href="css/DIV.css" rel="stylesheet">
+<body>
+
+
+
+<div id="top" >
+  <div id="top_left"></div>
+ 
+  <div id="top_center">
+    <div id="logo" align="center">
+  <img src="image/Untitled.jpg" alt="logo" style="width: 800px; height: 130px;" />
+    </div>
+    <div id="search" align="center">
+  <form > 
+  <table cellpadding="0px" cellspacing="0px">
+  <tr> 
+  <td style="border-style:solid none solid solid;border-color:#4B7B9F;border-width:1px;">
+  <input type="text" name="zoom_query" style="width:690px; border:0px solid; height:30px; padding:0px 3px; position:relative;"> 
+  </td>
+  <td style="border-style:solid;border-color:#4B7B9F;border-width:1px;"> 
+  <input type="image" src="image/images.jpg" alt="SUBMIT" name=" "style="width: 35px; height: 30px;" />
+  </td>
+  </tr>
+  </table>
+  </form>
+    </div>
+    <div id="menu" align="center">
+           <ul>
+        <li style="border-left : none;"><a href="tc.html"> TRANG CHỦ </a></li>
+        <li> <a href="cctc.html" >CƠ CẤU TỔ CHỨC</a> </li>
+        <li> <a href="qd.html" >QUY ĐỊNH</a></li>
+        <li> <a href="bm.html" >BIỂU MẪU</a></li>
+        <li><a href="lh.html" > LIÊN HỆ </a></li>
+            </ul>
+     </div>    
+  </div>
+   <div id="top_right" align="center" style="line-height:100px;">
+  <a href="dn.html"><button type="button">Đăng Nhập</button></a>
+  <a href="dk.html"><button type="button">Đăng Ký</button></a>
+   </div>
+</div>
+
+
+
+<div id="mid" >
+  <div id="mid_left" >
+    <div id="mid_left_top" align="center">
+      <ul>
+        <li style="border-top : none;border-left : none;background-color: #FFFFFF;"> <a style="color:#0000FF;" >LĨNH VỰC</a> </li>
+        <li style="border-top : none;"><a href="cntt.html" >CÔNG NGHỆ THÔNG TIN  </a></li>
+        <li> <a href="ddt.html" >ĐIỆN - ĐIỆN TỬ</a> </li>
+        <li> <a href="ckctm.html" >CƠ KHÍ CHẾ TẠO MAY</a></li>
+        <li><a href="hhtp.html" > HÓA HỌC & THỰC PHẨM</a></li>
+        <li><a href="llct.html" >LÝ LUẬN CHÍNH TRỊ</a></li>
+        <li><a href="khtn.html" >KHOA HOC TỰ NHIÊN </a></li>
+        <li><a href="th.html" > TRIẾT HỌC</a></li>
+                  </ul>
+    </div>
+    <div id="mid_left_bot" align="center">
+      THÔNG BÁO<br>
+      <a href="tb.html">thong bao</a>
+    </div>
+  </div>
+  <div id="mid_center" >
+      <ul>
+      <div class="new">
+       <a href="docbai.html"><img src="image/a.jpg" alt="logo" vspace="5px" hspace="5px" style="width: 130px; height: 130px;border: 1px solid #000000;" /><h1><br>Camera kép trên iPhone 7 Plus: Không  <br><br>phải là tính năng mới</h1></a>
+                  </div>
+      <div class="new">
+       <a href="#"><img src="image/b.jpg" alt="logo" vspace="5px" hspace="5px" style="width: 130px; height: 130px;border: 1px solid #000000;" /><h1><br>Google phát triển hệ điều hành Fuchsia  <br><br>"mới toanh"danh cho IoT </h1></a>
+                  </div>
+      <div class="new">
+       <a href="#"><img src="image/c.jpg" alt="logo" vspace="5px" hspace="5px" style="width: 130px; height: 130px;border: 1px solid #000000;" /><h1><br>Singapore đã trở thành quốc gia đầu<br> <br> tiên có taxi tự lái </h1></a>
+                  </div>
+      </ul>
+  </div>
+</div>
+
+
+
+<div id="bot" align="center">
+<pre>
+Copyright © 2013, Trường Đại Học Sư Phạm Kỹ Thuật - Tp.HCM
+Địa chỉ: 1 Võ Văn Ngân, Phường Linh Chiểu, Quận Thủ Đức, Thành phố Hồ Chí Minh.
+Điện thoại: (+84 - 8) 38968641 - (+84 - 8) 38961333
+E-mail: ic@hcmute.edu.vn
+</pre>
+</div>
+  </body>
+</html>
